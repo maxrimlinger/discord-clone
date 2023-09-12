ccc386bf98a13616968aba02e1cf7709eb2f295f
Max Rimlinger <max@netflint.com>
2023-09-12 15:53:17 -0400

    moving view, create, delete task functionality

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 from flask import Flask, render_template, url_for, request, redirect
+from google.cloud import datastore
 from datetime import datetime
 
 app = Flask(__name__)
+db = datastore.Client()
+
+next_task_id = 0
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content=task_content)
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an error adding your task'
+        task = db.Entity(db.key("task", next_task_id))
+        next_task_id += 1
+        task.update(
+            {
+                "content": task_content,
+                "date_created": datetime.datetime.utcnow(),
+            }
+        )
+    
+        return redirect('/')
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        query = db.query(kind="task")
+        query.order = ["date"]
+        tasks = list(query.fetch())
         return render_template('index.html', tasks=tasks)
     
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    # test
-    except:
-        return 'There was an error deleting your task'
+    db.delete(db.key("task", id))
+    return redirect('/')
     
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
