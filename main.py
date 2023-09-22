c649c57cfb81151992ee0b1407c30d6ad65235f9
Max Rimlinger <max@netflint.com>
2023-09-22 19:59:19 -0400

    doing more changes with timestamps

--- main.py
+++ main.py
@@ -1,50 +1,61 @@
 from flask import Flask, render_template, url_for, request, redirect
 from google.cloud import datastore
 from datetime import datetime, timezone, timedelta, date
-from zoneinfo import ZoneInfo
+import pytz
 
 app = Flask(__name__)
 db = datastore.Client()
 
 class Message():
+    """Represents a chat message. This class does the heavy lifting of
+    converting the date and time the message was sent to a more readible format
+    for the frontend.
+    """
     def __init__(self, content, datetime_sent):
         self.content = content
         self.datetime_sent = get_relational_datetime(datetime_sent)
 
 def get_relational_datetime(dt_message):
     now = datetime.now(timezone.utc)
-    td_since_message = now - dt_message
+    time_since_message = now - dt_message
+    # utc = pytz.timezone('UTC')
+    # now = utc.localize(datetime.utcnow())
+    tz = pytz.timezone("America/New_York") # TODO change this with user settings
+    local_dt_message = dt_message.astimezone(tz)
+    local_now = now.astimezone(tz)
 
+    dt_10_seconds_ago = now - timedelta(seconds=10)
     dt_1_minute_ago = now - timedelta(minutes=1)
     dt_2_minutes_ago = now - timedelta(minutes=2)
     dt_1_hour_ago = now - timedelta(hours=1)
     dt_2_hours_ago = now - timedelta(hours=2)
-    dt_beginning_of_today = datetime.combine(date.today(), datetime.min.time(), tzinfo=timezone.utc)
-    date_yesterday = date.today() - timedelta(days=1)
-    dt_beginning_of_yesterday = datetime.combine(date_yesterday, datetime.min.time(), tzinfo=timezone.utc)
 
-    # TODO time is only shown in UTC
-    # tzinfo=ZoneInfo("America/Los_Angeles")
+    dt_beginning_of_today = datetime(local_now.year, local_now.month, local_now.day, tzinfo=tz)
+    dt_beginning_of_yesterday = dt_beginning_of_today - timedelta(days=1)
+
+    if dt_message > dt_10_seconds_ago:
+        return "Now" + str(local_dt_message)
     if dt_message > dt_1_minute_ago:
-        return "Now" + str(dt_message)
-    elif dt_message > dt_2_minutes_ago:
-        return "1 minute ago" + str(dt_message)
-    elif dt_message > dt_1_hour_ago:
-        return str(td_since_message.seconds // 60) + " minutes ago" + str(dt_message)
-    elif dt_message > dt_2_hours_ago:
-        return "1 hour ago" + str(dt_message)
-    elif dt_message > dt_beginning_of_today:
-        return str(td_since_message.seconds // 3600) + " hours ago" + str(dt_message)
-    elif dt_message > dt_beginning_of_yesterday:
-        return "Yesterday"  + str(dt_message) # wack (maybe switch to discord method)
+        return "< 1 minute ago" + str(local_dt_message)
+    if dt_message > dt_2_minutes_ago:
+        return "1 minute ago" + str(local_dt_message)
+    if dt_message > dt_1_hour_ago:
+        return str(time_since_message.seconds // 60) + " minutes ago" + str(local_dt_message)
+    if dt_message > dt_2_hours_ago:
+        return "1 hour ago" + str(local_dt_message)
+    
+    if local_dt_message > dt_beginning_of_today:
+        return str(time_since_message.seconds // 3600) + " hours ago" + str(local_dt_message)
+    if local_dt_message > dt_beginning_of_yesterday:
+        return "Yesterday"  + str(local_dt_message)
     else:
-        return "old" + str(dt_message)
+        return "old" + str(local_dt_message)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == 'POST':
-        message_content = request.form['content']
-        if message_content.isspace() or message_content == "": return redirect('/')
+    if request.method == "POST":
+        message_content = request.form["content"]
+        if message_content.isspace() or message_content == "": return redirect("/")
         message = datastore.Entity(db.key("message"))
         message.update(
             {
@@ -54,7 +65,7 @@ def index():
         )
         db.put(message)
     
-        return redirect('/')
+        return redirect("/")
     else:
         query = db.query(kind="message")
         query.order = ["datetime_sent"]
@@ -63,12 +74,12 @@ def index():
         for message in messages:
             formatted_message = Message(message["content"], message["datetime_sent"])
             formatted_messages.append(formatted_message)
-        return render_template('index.html', messages=formatted_messages)
+        return render_template("index.html", messages=formatted_messages)
     
-@app.route('/delete/<int:id>')
+@app.route("/delete/<int:id>")
 def delete(id):
     db.delete(db.key("message", id))
-    return redirect('/')
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
