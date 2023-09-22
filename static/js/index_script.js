16ab4cd1f46343fc364d96d9253c48996253e12d
Max Rimlinger <mwr9727@rit.edu>
2023-09-22 15:02:54 -0400

    big layout changes that i forgot to do

---
+++ static/js/index_script.js
@@ -0,0 +1,4 @@
+var element = document.getElementsByClassName("message-table-container").item(0);
+element.scrollTop = element.scrollHeight;
+
+console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
