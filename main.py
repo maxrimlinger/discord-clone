65f29287256459bbbfb27e1572d88fa8b49f59ff
Max Rimlinger <mwr9727@rit.edu>
2023-09-29 12:54:32 -0400

    ui prep for adding channels

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ class Message():
 def get_relational_datetime(dt_message):
     now = datetime.now(timezone.utc)
     time_since_message = now - dt_message
-    # utc = pytz.timezone('UTC')
-    # now = utc.localize(datetime.utcnow())
     tz = pytz.timezone("America/New_York") # TODO change this with user settings
     local_dt_message = dt_message.astimezone(tz)
     local_now = now.astimezone(tz)
@@ -30,26 +28,28 @@ def get_relational_datetime(dt_message):
     dt_1_hour_ago = now - timedelta(hours=1)
     dt_2_hours_ago = now - timedelta(hours=2)
 
+    # local to user
     dt_beginning_of_today = datetime(local_now.year, local_now.month, local_now.day, tzinfo=tz)
     dt_beginning_of_yesterday = dt_beginning_of_today - timedelta(days=1)
+    days_since_message = dt_beginning_of_today - local_dt_message + timedelta(days=1)
 
     if dt_message > dt_10_seconds_ago:
-        return "Now" + str(local_dt_message)
+        return "Now"
     if dt_message > dt_1_minute_ago:
-        return "< 1 minute ago" + str(local_dt_message)
+        return "< 1 minute ago"
     if dt_message > dt_2_minutes_ago:
-        return "1 minute ago" + str(local_dt_message)
+        return "1 minute ago"
     if dt_message > dt_1_hour_ago:
-        return str(time_since_message.seconds // 60) + " minutes ago" + str(local_dt_message)
+        return str(time_since_message.seconds // 60) + " minutes ago"
     if dt_message > dt_2_hours_ago:
-        return "1 hour ago" + str(local_dt_message)
+        return "1 hour ago"
     
     if local_dt_message > dt_beginning_of_today:
-        return str(time_since_message.seconds // 3600) + " hours ago" + str(local_dt_message)
+        return str(time_since_message.seconds // 3600) + " hours ago"
     if local_dt_message > dt_beginning_of_yesterday:
-        return "Yesterday"  + str(local_dt_message)
+        return "Yesterday" 
     else:
-        return "old" + str(local_dt_message)
+        return str(days_since_message.days) + " days ago"
 
 @app.route("/", methods=["POST", "GET"])
 def index():
