c14ef8be85aa573282703d1b30fadfd4d8dc0c60
Max Rimlinger <mwrimlinger@gmail.com>
2023-09-30 02:23:50 -0400

    channels is resizable

--- static/js/index_script.js
+++ static/js/index_script.js
@@ -1,2 +1,24 @@
-var element = document.getElementById("chat-list-container");
-element.scrollTop = element.scrollHeight;
+const chat_list_container = document.getElementById("chat-list-container");
+chat_list_container.scrollTop = chat_list_container.scrollHeight;
+
+function m_move_divider(e) {
+    e.preventDefault();
+    const grid = document.getElementById("content");
+    grid.style["grid-template-columns"] = e.clientX + "px 2px 1fr"
+}
+
+function m_down_divider(e) {
+    e.preventDefault();
+    document.addEventListener("mousemove", m_move_divider, true);
+    console.log("down");
+}
+
+function m_up_divider(e) {
+    e.preventDefault();
+    document.removeEventListener("mousemove", m_move_divider, true);
+    console.log("up");
+}
+
+const divider = document.getElementById("pane-divider");
+divider.addEventListener("mousedown", m_down_divider, true);
+document.addEventListener("mouseup", m_up_divider, true);
