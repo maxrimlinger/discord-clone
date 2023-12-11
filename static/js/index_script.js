63f098315cc67cbdeb84383d28da6dd43e7ebb7f
Max Rimlinger <37249275+maxwrimlinger@users.noreply.github.com>
2023-12-11 11:44:22 -0500

    started implemented google oauth and also added a favicon

--- static/js/index_script.js
+++ static/js/index_script.js
@@ -1,3 +1,18 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+}
+
 function auto_scroll_bottom() {
     const chat_list_container = document.getElementById("chat-list-container");
     if (chat_list_container !== null) {
