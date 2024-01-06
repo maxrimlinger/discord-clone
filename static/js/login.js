fcb0e15e3409e48ce50516dc796e8bac93a1c7d4
Max Rimlinger <37249275+maxwrimlinger@users.noreply.github.com>
2024-01-05 22:33:24 -0800

    made login page a little simpler

--- static/js/login.js
+++
@@ -1,5 +0,0 @@
-// prevent the google sign-in button from actually doing google sign-in
-document.querySelector(".g_id_signin").addEventListener("click", function(e) {
-    e.preventDefault();
-    window.location.href = "/login";
-}, true);
