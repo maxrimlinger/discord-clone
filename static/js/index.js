4d485e3c7fa6cea897d11ce8ae70eadf25d44d8d
Max Rimlinger <37249275+maxwrimlinger@users.noreply.github.com>
2024-01-06 23:22:13 -0800

    preliminary testing for edit message dialog box

--- static/js/index.js
+++ static/js/index.js
@@ -39,5 +39,33 @@ function setup_divider() {
     document.addEventListener("mouseup", m_up_divider, true);
 }
 
+function toggle_message_dialog(id) {
+    const dialog = document.getElementById("message-dialog");
+    if (dialog.open) {
+        dialog.close();
+    } else {
+        const message = document.getElementById(`${id}-message`)
+        const messageRect = message.getBoundingClientRect();
+
+        dialog.style.bottom = `${window.innerHeight - messageRect.y}px`;
+        dialog.style.left = "0";
+        dialog.show();
+    }
+}
+
+function setup_message_options_buttons() {
+    document.body.addEventListener("click", function() {
+        document.getElementById("message-dialog").close();
+    })
+    const options_buttons = document.getElementsByClassName("options-button")
+    for (let i = 0; i < options_buttons.length; i++) {
+        options_buttons[i].addEventListener("click", (e) => {
+            e.stopPropagation();
+            toggle_message_dialog(options_buttons[i].id.slice(0, -15))
+        })
+    }
+}
+
 auto_scroll_bottom();
 setup_divider();
+setup_message_options_buttons();
